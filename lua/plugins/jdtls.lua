0abd173aa8b5b8e457749a22ec5cc4590ddeb3c7
James Taylor <james.taylor2@comcast.net>
2025-10-17 18:15:38 -0700

    Verified that jdtls uses google style java formatting

--- lua/plugins/jdtls.lua
+++ lua/plugins/jdtls.lua
@@ -15,10 +15,19 @@
 
 return {
   {
-    "mfussenegger/nvim-jdtls",
-    ft = "java",
-    dependencies = {
-      "neovim/nvim-lspconfig",
+    "nvim-jdtls",
+    opts = {
+      settings = {
+        java = {
+          format = {
+            enabled = true,
+            settings = {
+              url = "https://raw.githubusercontent.com/google/styleguide/gh-pages/eclipse-java-google-style.xml",
+              profile = "GoogleStyle",
+            },
+          },
+        },
+      },
     },
   },
 }
